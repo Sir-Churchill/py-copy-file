c418e1dff180810a92ca78339cd80b13d1f773f6
Nik <your@mail.com>
2025-07-14 00:29:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(files: str) -> None:
+    ls = files.split()
+
+    if len(ls) != 3 or ls[0] != "cp" or ls[1] == ls[2]:
+        return None
+    else:
+        try:
+            with (
+                open(ls[1], "r") as file_in,
+                open(ls[2], "w") as file_out
+            ):
+                file_out.write(file_in.read())
+        except FileNotFoundError:
+            return None
